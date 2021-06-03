09d0c1a0590ba7204347f561b72601337220ea11
gamecult2 <74660921+gamecult2@users.noreply.github.com>
2021-06-03 17:13:04 +0800

    Update chimney.py

--- chimney.py
+++ chimney.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-# SET UP ----------------------------------------------------------------------------
+# SET UP -----------------------------------------------------
 wipe()  # clear opensees model
 model('basic', '-ndm', 2, '-ndf', 3)  # 2 dimensions, 3 dof per node
 
@@ -61,9 +61,9 @@ ftCC30 = -0.14 * fc1CC30  # tensile strength +tension
 ftUC30 = -0.14 * fc1UC30 # tensile strength +tension
 EtsC30 = ftUC30 / 0.002  # tension softening stiffness
 
-# -----------------------------------------------------
-# ----------------------- STEEL -----------------------
-# -----------------------------------------------------
+# ------------------------------------------------------------
+# ----------------------- STEEL ------------------------------
+# ------------------------------------------------------------
 IDreinf = 5  # material ID tag -- reinforcement
 
 Fy = 413.7  # STEEL yield stress
@@ -74,46 +74,37 @@ cR1 = 0.925  # control the transition from elastic to plastic branches
 cR2 = 0.15	# control the transition from elastic to plastic branches
 
 # Define materials for nonlinear columns
-# ------------------------------------------
+# -------------------------------------------------------------------
 # CORE CONCRETE C40 (confined)
 uniaxialMaterial('Concrete02', IDconcCoreC40, fc1CC40, eps1CC40, fc2CC40, eps2CC40, lamC40, ftCC40, EtsC40)
 # COVER CONCRETE C40 (unconfined)
 uniaxialMaterial('Concrete02', IDconcCoverC40, fc1CC40, eps1CC40, fc2CC40, eps2CC40, lamC40, ftCC40, EtsC40)
-# ------------------------------------------
+# ------------------------------------------------------------------
 # CORE CONCRETE C30 (confined)
 uniaxialMaterial('Concrete02', IDconcCoreC30, fc1CC30, eps1CC30, fc2CC30, eps2CC30, lamC30, ftCC30, EtsC30)
 # COVER CONCRETE C30 (unconfined)
 uniaxialMaterial('Concrete02', IDconcCoverC30, fc1CC30, eps1CC30, fc2CC30, eps2CC30, lamC30, ftCC30, EtsC30)
-# ------------------------------------------
+# ------------------------------------------------------------------
 # STEEL Reinforcing steel
 uniaxialMaterial('Steel02', IDreinf, Fy, Es, Bs, R0, cR1, cR2)
+# END MATERIAL parameters ------------------------------------------
 
-# END MATERIAL parameters -------------------------------------------------------------------
 
-# START SECTION parameters -----------------------------------------------------------------
-# section GEOMETRY -------------------------------------------------------------
+# ------------------------------------------------------------------
+# ----------------------- Different GEOMETRY Coordinate-------------
+# ------------------------------------------------------------------
 #                          1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24
-element_length = np.array([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
-                           4])  # Number of element 25
+element_length = np.array([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 4])  # Number of element 25
 #                              1  2   3   4   5   6   7   8   9   10  11   12   13   14   15   16   17   18   19   20   21   22   23   24   25
-Nodes_coordinate_Y = np.array(
-    [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 234])
+Nodes_coordinate_Y = np.array([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 234])
 # Section Diameter ---------------         1     2     3     4     5     6     7     8     9     10    11    12    13    14    15    16    17    18    19    20    21    22    23    24
-DSec = Column_Section_Diameter = np.array(
-    [27.3, 26.7, 26.1, 25.5, 24.9, 24.4, 23.7, 23.5, 23.1, 22.7, 22.3, 22.1, 21.9, 21.7, 21.5, 21.3, 21.1, 20.9, 20.9,
-     20.9, 20.9, 20.9, 20.9, 20.9])
+DSec = Column_Section_Diameter = np.array([27.3, 26.7, 26.1, 25.5, 24.9, 24.4, 23.7, 23.5, 23.1, 22.7, 22.3, 22.1, 21.9, 21.7, 21.5, 21.3, 21.1, 20.9, 20.9, 20.9, 20.9, 20.9, 20.9, 20.9])
 # Section Thickness ---------------         1     2     3     4     5     6     7     8     9     10    11    12    13    14    15    16    17    18    19    20    21    22    23    24
-TSec = Column_Section_Thickness = np.array(
-    [0.75, 0.73, 0.71, 0.69, 0.67, 0.65, 0.65, 0.65, 0.63, 0.60, 0.57, 0.55, 0.53, 0.51, 0.49, 0.45, 0.43, 0.41, 0.39,
-     0.39, 0.39, 0.35, 0.35, 0.35])
+TSec = Column_Section_Thickness = np.array([0.75, 0.73, 0.71, 0.69, 0.67, 0.65, 0.65, 0.65, 0.63, 0.60, 0.57, 0.55, 0.53, 0.51, 0.49, 0.45, 0.43, 0.41, 0.39, 0.39, 0.39, 0.35, 0.35, 0.35])
 
 # Vertical reinforcement on the outer side of the tube wall (HRB400 grade is adopted for the reinforcement)
-Rebar_spacing_VO = np.array(
-    [0.205, 0.205, 0.205, 0.215, 0.215, 0.225, 0.225, 0.225, 0.225, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235,
-     0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235])
-Rebar_diameter_VO = np.array(
-    [0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022,
-     0.020, 0.020, 0.020, 0.020, 0.020, 0.018, 0.018, 0.018, 0.018, 0.018, 0.018, 0.018])
+Rebar_spacing_VO = np.array([0.205, 0.205, 0.205, 0.215, 0.215, 0.225, 0.225, 0.225, 0.225, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235, 0.235])
+Rebar_diameter_VO = np.array([0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.022, 0.020, 0.020, 0.020, 0.020, 0.020, 0.018, 0.018, 0.018, 0.018, 0.018, 0.018, 0.018])
 
 # Vertical reinforcement on the inner side of the tube wall (HRB400 grade is adopted for the reinforcement)
 Rebar_spacing_VI = np.full(len(element_length), 0.200)
@@ -177,7 +168,6 @@ for i in range(1, len(element_length)):
 # ----------------------- Define FIBER SECTION properties ---------------------------
 # -----------------------------------------------------------------------------------
 # Define the fiber section for Circular section
-
 for i in range(1, len(element_length)):
     section('Fiber', ColSecTag[i])
     patch('circ', IDconcCoverC40, nfCoverT[i], nfCoverR, 0, 0, rc[i], ro[i], 0, 360)  # Define the cover patch
@@ -185,9 +175,7 @@ for i in range(1, len(element_length)):
     patch('circ', IDconcCoreC40, nfCoreT[i], nfCoreR, 0., 0., rc2[i], rc[i], 0., 360)  # Define the core patch
     layer('circ', IDreinf, numBarsSec[i], barAreaSec[i], 0., 0., rc2[i], theta[i], 360)  # Define the reinforcing layer
     patch('circ', IDconcCoverC40, nfCoverT[i], nfCoverR, 0., 0., ri[i], rc2[i], 0., 360)  # Define the cover patch
-
-# END SECTION parameters -------------------------------------------------------------------
-
+# END FIBER SECTION properties -------------------------------------------------------
 
 # -----------------------------------------------------------------------------------
 # ----------------------- Define GEOMETRY properties --------------------------------
@@ -199,7 +187,9 @@ for i in range(0, len(element_length)):
 # Single point constraints -- Boundary Conditions
 fix(1, 1, 1, 1)  # node DX DY RZ
 
-# Define ELEMENTS -------------------------------------------------------------
+# -----------------------------------------------------------------------------------
+# ----------------------- Define ELEMENTS properties --------------------------------
+# -----------------------------------------------------------------------------------
 # define geometric transformation: performs a linear geometric transformation of beam stiffness and resisting force from the basic system to the global-coordinate system
 ColTransfTag = 1
 geomTransf('PDelta',  ColTransfTag)  		             # associate a tag to transformation
@@ -208,16 +198,14 @@ for i in range(0, len(element_length)):
     print('nonlinearBeamColumn', i, *[i, i+1], 5, ColSecTag[i], 1)
     element('nonlinearBeamColumn', i, *[i, i+1], 5, ColSecTag[i], 1)
 
-
 # fib_sec = [['section', 'Fiber', 1, '-GJ', 1.0e6],
 #             ['patch', 'circ', IDconcCoverC40, nfCoverT[i], nfCoverR, 0., 0., rc[i], ro[i], 0., 360],
-#              ['layer', 'circ', IDreinf, numBarsSec[i], barAreaSec[i], 0., 0., rc[i], theta[i], 360],
-#              ['patch', 'circ', IDconcCoreC40, nfCoreT[i], nfCoreR, 0., 0., rc2[i], rc[i], 0., 360],
-#              ['layer', 'circ', IDreinf, numBarsSec[i], barAreaSec[i], 0., 0., rc2[i], theta[i], 360],
-#              ['patch', 'circ', IDconcCoverC40, nfCoverT[i], nfCoverR, 0., 0., ri[i], rc2[i], 0., 360]]
-
-    #     matcolor = ['r', 'lightgrey', 'gold', 'w', 'w', 'w']
-    #     opsv.plot_fiber_section(fib_sec, matcolor=matcolor)
-    #    plt.axis('equal')
-    # plt.savefig('fibsec_rc.png')
-#    plt.show()
+#             ['layer', 'circ', IDreinf, numBarsSec[i], barAreaSec[i], 0., 0., rc[i], theta[i], 360],
+#             ['patch', 'circ', IDconcCoreC40, nfCoreT[i], nfCoreR, 0., 0., rc2[i], rc[i], 0., 360],
+#             ['layer', 'circ', IDreinf, numBarsSec[i], barAreaSec[i], 0., 0., rc2[i], theta[i], 360],
+#             ['patch', 'circ', IDconcCoverC40, nfCoverT[i], nfCoverR, 0., 0., ri[i], rc2[i], 0., 360]]
+# matcolor = ['r', 'lightgrey', 'gold', 'w', 'w', 'w']
+# opsv.plot_fiber_section(fib_sec, matcolor=matcolor)
+# plt.axis('equal')
+# plt.savefig('fibsec_rc.png')
+# plt.show()
